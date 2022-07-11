d6545b071360448f79502ba2217b80b54a29bf95
AlRezn <alrezn@gmail.com>
2022-07-11 13:43:19 +0300

    Reflection

--- 4.JavaCollections/src/com/javarush/task/task34/task3408/Cache.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3408/Cache.java
@@ -7,15 +7,28 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 public class Cache<K, V> {
-    private Map<K, V> cache = null;   //TODO add your code here
+    private Map<K, V> cache = new WeakHashMap<>();   //TODO add your code here
 
     public V getByKey(K key, Class<V> clazz) throws Exception {
         //TODO add your code here
-        return null;
+        if (!cache.containsKey(key)) {
+            Constructor<V> constructor = clazz.getDeclaredConstructor(key.getClass());
+            cache.put(key, constructor.newInstance(key));
+        }
+        return cache.get(key);
     }
 
-    public boolean put(V obj) {
+    public boolean put(V obj) throws IllegalAccessException {
         //TODO add your code here
+        try {
+            Method method = obj.getClass().getDeclaredMethod("getKey", null);
+            method.setAccessible(true);
+            K key = (K) method.invoke(obj);
+            cache.put(key, obj);
+            return true;
+        } catch (NoSuchMethodException | InvocationTargetException e) {
+        } catch (IllegalAccessException e) {
+        }
         return false;
     }
 
