54e2e3ad921a3f3c9840d1c20097bf50d729f35e
AlRezn <alrezn@gmail.com>
2022-07-11 21:29:44 +0300

    set(4)

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,21 +1,57 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    @Override
+    public Object clone() throws InternalError {
+        AmigoSet amigoSet;
+        try {
+            amigoSet = (AmigoSet) super.clone();
+            amigoSet.map = (HashMap) map.clone();
+        } catch (Exception e) {
+            throw new InternalError();
+        }
+        return amigoSet;
+    }
 
 
     @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
+
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        this.map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
+        addAll(collection);
+    }
+
+    @Override
     public Iterator<E> iterator() {
-        return null;
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
+    }
+
+    @Override
+    public boolean add(E e) {
+        return null == map.put(e, PRESENT);
     }
 }
