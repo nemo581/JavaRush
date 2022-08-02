6413176c2101f9084e6770ab5fccd9065cb5473a
AlRezn <alrezn@gmail.com>
2022-08-02 21:18:50 +0300

    .

--- 4.JavaCollections/src/com/javarush/task/task35/task3501/GenericStatic.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/GenericStatic.java
@@ -3,6 +3,6 @@ package com.javarush.task.task35.task3501;
 public class GenericStatic {
     public static <Object> Object someStaticMethod(Object genericObject) {
         System.out.println(genericObject);
-        return (Object) genericObject;
+        return genericObject;
     }
 }
