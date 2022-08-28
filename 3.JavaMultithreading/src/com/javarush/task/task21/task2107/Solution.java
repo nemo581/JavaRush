a00a8ca97681a21f914d8e0ca5a06be2dba0c5ff
AlRezn <alrezn@gmail.com>
2022-08-28 18:27:10 +0300

    Глубокое клонирование карты_Closed

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -2,20 +2,17 @@ package com.javarush.task.task21.task2107;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 /* 
 Глубокое клонирование карты
 */
-
-public class Solution {
-
+public class Solution implements Cloneable {
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.users.put("Hubert", new User(172, "Hubert"));
         solution.users.put("Zapp", new User(41, "Zapp"));
         Solution clone = null;
         try {
-            clone = solution.clone();
+            clone = (Solution) solution.clone();
             System.out.println(solution);
             System.out.println(clone);
 
@@ -29,7 +26,7 @@ public class Solution {
 
     protected Map<String, User> users = new LinkedHashMap();
 
-    public static class User {
+    public static class User implements Cloneable {
         int age;
         String name;
 
@@ -37,5 +34,42 @@ public class Solution {
             this.age = age;
             this.name = name;
         }
+
+        @Override
+        public Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            User user = (User) o;
+
+            if (age != user.age) return false;
+            return name != null ? name.equals(user.name) : user.name == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = age;
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
+    }
+
+
+    public Object clone() throws CloneNotSupportedException {
+        Solution o = (Solution) super.clone();
+
+        Map<String, User> newUsers = new LinkedHashMap<>();
+        for (String key : o.users.keySet()) {
+            User user = o.users.get(key);
+            newUsers.put(key, (User) user.clone());
+        }
+        o.users = newUsers;
+
+        return o;
     }
 }
