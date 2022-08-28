568cfcb1c1b6f19303f83f78d5145ee7aff26dbe
AlRezn <alrezn@gmail.com>
2022-08-28 14:56:29 +0300

    public Human getPerson(int age) {

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,12 +1,18 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.female.FemaleFactory;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
 public class Solution {
     public static void main(String[] args) {
         MaleFactory maleFactory = new MaleFactory();
+        FemaleFactory femaleFactory = new FemaleFactory();
         System.out.println(maleFactory.getPerson(99));
         System.out.println(maleFactory.getPerson(4));
         System.out.println(maleFactory.getPerson(15));
+
+        System.out.println(femaleFactory.getPerson(99));
+        System.out.println(femaleFactory.getPerson(4));
+        System.out.println(femaleFactory.getPerson(15));
     }
 }
