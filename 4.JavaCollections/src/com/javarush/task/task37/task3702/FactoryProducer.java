47d7340a5624d63a5a238e0237158a4b0f293ba4
AlRezn <alrezn@gmail.com>
2022-08-28 15:51:15 +0300

    .

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -6,16 +6,10 @@ import com.javarush.task.task37.task3702.male.MaleFactory;
 public class FactoryProducer {
     enum HumanFactoryType { MALE, FEMALE };
     public static AbstractFactory getFactory(HumanFactoryType factoryType) {
-        switch (factoryType) {
-            case FEMALE: return new FemaleFactory();
-            case MALE: return new MaleFactory();
-            default: return null;
+        if (factoryType == HumanFactoryType.MALE) {
+            return new MaleFactory();
+        } else {
+            return new FemaleFactory();
         }
-
-//        if (factoryType == HumanFactoryType.MALE) {
-//            return new MaleFactory();
-//        } else {
-//            return new FemaleFactory();
-//        }
     }
 }
