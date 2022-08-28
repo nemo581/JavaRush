13eae9257d31a7ef4bc35f28399690d2fc1a6544
AlRezn <alrezn@gmail.com>
2022-08-28 19:17:51 +0300

    Клонирование растений_Closed

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
         Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
         Tree clone = null;
         try {
-            clone = tree.clone();
+            clone = (Tree) tree.clone();
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
         }
@@ -17,7 +17,6 @@ public class Solution {
         System.out.println(tree);
         System.out.println(clone);
 
-        System.out.println(tree.branches);
         System.out.println(clone.branches);
     }
 
@@ -33,7 +32,7 @@ public class Solution {
         }
     }
 
-    public static class Tree extends Plant {
+    public static class Tree extends Plant implements Cloneable {
         private String[] branches;
 
         public Tree(String name, String[] branches) {
@@ -43,6 +42,37 @@ public class Solution {
 
         public String[] getBranches() {
             return branches;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            Tree cloneTree = (Tree) super.clone();
+            String[] cloneBranches = new String[cloneTree.getBranches().length];
+            for (int i = 0; i < cloneTree.getBranches().length; i++) {
+                cloneBranches[i] = cloneTree.getBranches()[i];
+            }
+            cloneTree.branches = cloneBranches;
+            return cloneTree;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || this.getClass() != obj.getClass()) return false;
+            Tree tree = (Tree) obj;
+            if (this.branches.length != tree.branches.length) return false;
+            if (!this.getName().equals(tree.getName())) return false;
+            for (int i = 0; i < this.branches.length; i++) {
+                if (!this.branches[i].equals(tree.branches[i])) return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = this.branches.length;
+            result = 31 * result + (this.branches != null ? this.branches.hashCode() : 0);
+            return result;
         }
     }
 }
