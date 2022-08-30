e5560a7aa55b68b82daa235603fae6753946f1d5
AlRezn <alrezn@gmail.com>
2022-08-30 20:17:52 +0300

    _CLOSED_Ипподром(8)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -15,7 +15,15 @@ public class Hippodrome {
     }
 
     public void run() {
-
+        for (int i = 1; i <= 100; i++) {
+            move();
+            print();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void move() {
