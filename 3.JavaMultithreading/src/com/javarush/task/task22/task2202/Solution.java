970cab85bf795074391c0ddc842930b3380ffdc4
AlRezn <alrezn@gmail.com>
2022-09-06 01:11:37 +0300

    CLOSED_Найти подстроку

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -6,13 +6,55 @@ package com.javarush.task.task22.task2202;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java"));
+
+//        T-E-S-T
+//        String s = null;
+//        for (int j = 0; j < 10; j++) {
+//            long startTime = System.currentTimeMillis();
+//            for (int i = 0; i < 1000000; i++) {
+//                System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java"));
+//                s = getPartOfString("JavaRush - лучший сервис обучения Java");
+//            }
+//            System.out.println(System.currentTimeMillis() - startTime);
+//        }
     }
 
     public static String getPartOfString(String string) {
-        return null;
+//        MuSolution
+        int index = -1;
+        if (string == null || !string.contains(" ")) {
+            throw new TooShortStringException();
+        } else {
+            for (int i = 0; i < 4; i++) {
+                index = string.indexOf(" ", index + 1);
+                if (index == -1) {
+                    throw new TooShortStringException();
+                }
+            }
+            if (string.indexOf(" ", index + 1) != -1) {
+                return string.substring(string.indexOf(" ") + 1, string.indexOf(" ", index + 1));
+            } else {
+                return string.substring(string.indexOf(" ") + 1);
+            }
+        }
+
+//        TheirSolution
+//        if (string == null) {
+//            throw new TooShortStringException();
+//        }
+//        String[] splitWithSpaces = string.split(" ");
+//
+//        if (splitWithSpaces.length < 5) throw new TooShortStringException();
+//        StringBuilder stringBuilder = new StringBuilder();
+//
+//        for (int i = 1; i < 5; i++) {
+//            stringBuilder.append(splitWithSpaces[i]);
+//            stringBuilder.append(" ");
+//        }
+//        return stringBuilder.toString().trim();
     }
 
-    public static class TooShortStringException {
+    public static class TooShortStringException extends RuntimeException {
     }
 }
