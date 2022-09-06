29181308ff22788611b96943430002ef1e989e61
AlRezn <alrezn@gmail.com>
2022-09-06 22:08:16 +0300

    CLOSED_Между табуляциями

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -5,8 +5,28 @@ package com.javarush.task.task22.task2203;
 */
 
 public class Solution {
-    public static String getPartOfString(String string) {
-        return null;
+    private static int count = 0;
+
+    public static String getPartOfString(String string) throws TooShortStringException {
+        int index;
+
+        try {
+            index = string.indexOf("\t");
+        } catch (NullPointerException n) {
+            throw new TooShortStringException();
+        }
+
+        if (index != -1) {
+            count++;
+            if (count == 2) {
+                count = 0;
+                return string.substring(0, index);
+            }
+            return getPartOfString(string.substring(index + 1));
+        } else {
+            count = 0;
+            throw new TooShortStringException();
+        }
     }
 
     public static class TooShortStringException extends Exception {
