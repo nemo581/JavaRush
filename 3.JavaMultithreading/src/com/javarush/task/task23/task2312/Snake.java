4d2e7f5658b7abad09277e8d6f6a7bc94df8abf0
AlRezn <alrezn@gmail.com>
2022-12-06 18:18:38 +0300

    Змейка(8)

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,10 +1,11 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
     private List<SnakeSection> sections;
-    private boolean isAlive;
+    private boolean isAlive = true;
     private SnakeDirection direction;
 
     public List<SnakeSection> getSections() {
@@ -21,5 +22,21 @@ public class Snake {
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
+    }
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+    }
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public void move() {
+
     }
 }
