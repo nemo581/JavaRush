f0b8a7e4092097a0a97451e921e7d2f817b469bd
AlRezn <alrezn@gmail.com>
2023-01-10 01:13:11 +0300

    task3411_Ханойские башни

--- 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
@@ -9,8 +9,17 @@ public class Solution {
         int numRings = 3;
         moveRing('A', 'B', 'C', numRings);
     }
-
     public static void moveRing(char a, char b, char c, int numRings) {
         //напишите тут ваш код
+        if (numRings == 1) {
+            print(a, b);
+        } else {
+            moveRing(a, c, b, numRings - 1);
+            print(a, b);
+            moveRing(c, b, a, numRings - 1);
+        }
+    }
+    public static void print(char p1, char p2) {
+        System.out.println("from " + p1 + " to " + p2);
     }
 }
