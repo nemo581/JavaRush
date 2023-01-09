1c905e853aad3ff86c40d312bc4e287101f7136e
AlRezn <alrezn@gmail.com>
2023-01-09 18:50:40 +0300

    task2413_Арканоид(7)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,4 +1,7 @@
 package com.javarush.task.task24.task2413;
 
 public class Canvas extends BaseObject {
+    public Canvas(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
