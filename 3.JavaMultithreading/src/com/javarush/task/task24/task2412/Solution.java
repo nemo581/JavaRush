d37d3561e586547df6817bafae4634bddc518151
AlRezn <alrezn@gmail.com>
2023-01-12 22:55:45 +0300

    >> Р:А:З:О:Б:Р:А:Т:Ь <<: task2412_Знания - сила!

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2412/Solution.java
@@ -0,0 +1,138 @@
+package com.javarush.task.task24.task2412;
+
+import java.text.ChoiceFormat;
+import java.text.Format;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
+
+/* 
+Знания - сила!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Stock> stocks = getStocks();
+        sort(stocks);
+        Date actualDate = new Date();
+        printStocks(stocks, actualDate);
+    }
+
+    public static void printStocks(List<Stock> stocks, Date actualDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+        double[] filelimits = {0d, actualDate.getTime()};
+        String[] filepart = {"change {4}", "open {2} and last {3}"};
+
+        ChoiceFormat fileform = new ChoiceFormat(filelimits, filepart);
+        Format[] testFormats = {null, null, dateFormat, fileform};
+        MessageFormat pattform = new MessageFormat("{0}   {1} | {5} {6}");
+        pattform.setFormats(testFormats);
+
+        for (Stock stock : stocks) {
+            String name = ((String) stock.get("name"));
+            String symbol = (String) stock.get("symbol");
+            double open = !stock.containsKey("open") ? 0 : ((double) stock.get("open"));
+            double last = !stock.containsKey("last") ? 0 : ((double) stock.get("last"));
+            double change = !stock.containsKey("change") ? 0 : ((double) stock.get("change"));
+            Date date = (Date) stock.get("date");
+            Object[] testArgs = {name, symbol, open, last, change, date, date.getTime()};
+            System.out.println(pattform.format(testArgs));
+        }
+    }
+
+    public static void sort(List<Stock> list) {
+//        ArrayList<String> sortName = new ArrayList<>();
+//        for (Stock s : list) {
+//            sortName.add(s.get("name").toString());
+//        }
+//        Collections.sort(sortName);
+//        for (String s : sortName) {
+//            for (Stock stock : list) {
+//                if (stock.get("name").equals(s)) {
+//                    System.out.println(stock);
+//                }
+//            }
+//        }
+
+        class StockNameComparator implements Comparator<Stock> {
+            public int compare(Stock a, Stock b) {
+                return a.get("name").toString().compareTo(b.get("name").toString());
+            }
+        }
+        class StockDateComparator implements Comparator<Stock> {
+            public int compare(Stock a, Stock b) {
+                Date DateA = (Date) a.get("date"), DateB = (Date) b.get("date");
+                LocalDate localDate1 = LocalDate.of(DateA.getYear() + 1900, DateA.getMonth() + 1, DateA.getDay() == 0 ? 7 : DateA.getDay());
+                LocalDate localDate2 = LocalDate.of(DateB.getYear() + 1900, DateB.getMonth() + 1, DateB.getDay() == 0 ? 7 : DateB.getDay());
+                return (-1) * localDate1.compareTo(localDate2);
+            }
+        }
+        class StockMoneyComparator implements Comparator<Stock> {
+            public int compare(Stock a, Stock b) {
+                double change1;
+                double change2;
+                if (!a.containsKey("change")) change1 = (Double) a.get("last") - (Double) a.get("open");
+                else change1 = (double) a.get("change");
+                if (!b.containsKey("change")) change2 = (Double) b.get("last") - (Double) b.get("open");
+                else change2 = (double) b.get("change");
+                return (-1) * Double.compare(change1, change2);
+            }
+        }
+        list.sort(new StockMoneyComparator());
+        list.sort(new StockDateComparator());
+        list.sort(new StockNameComparator());
+    }
+
+    public static class Stock extends HashMap<String, Object> {
+        public Stock(String name, String symbol, double open, double last) {
+            put("name", name);
+            put("symbol", symbol);
+            put("open", open);
+            put("last", last);
+            put("date", getRandomDate(java.time.LocalDate.now().getYear() + 1));
+        }
+
+        public Stock(String name, String symbol, double change, Date date) {
+            put("name", name);
+            put("symbol", symbol);
+            put("date", date);
+            put("change", change);
+        }
+    }
+
+    public static List<Stock> getStocks() {
+        List<Stock> stocks = new ArrayList<>();
+
+        stocks.add(new Stock("Fake Apple Inc.", "AAPL", 125.64, 123.43));
+        stocks.add(new Stock("Fake Cisco Systems, Inc.", "CSCO", 25.84, 26.3));
+        stocks.add(new Stock("Fake Google Inc.", "GOOG", 516.2, 512.6));
+        stocks.add(new Stock("Fake Intel Corporation", "INTC", 21.36, 21.53));
+        stocks.add(new Stock("Fake Level 3 Communications, Inc.", "LVLT", 5.55, 5.54));
+        stocks.add(new Stock("Fake Microsoft Corporation", "MSFT", 29.56, 29.72));
+
+        stocks.add(new Stock("Fake Nokia Corporation (ADR)", "NOK", .1, getRandomDate()));
+        stocks.add(new Stock("Fake Oracle Corporation", "ORCL", .15, getRandomDate()));
+        stocks.add(new Stock("Fake Starbucks Corporation", "SBUX", .03, getRandomDate()));
+        stocks.add(new Stock("Fake Yahoo! Inc.", "YHOO", .32, getRandomDate()));
+        stocks.add(new Stock("Fake Applied Materials, Inc.", "AMAT", .26, getRandomDate()));
+        stocks.add(new Stock("Fake Comcast Corporation", "CMCSA", .5, getRandomDate()));
+        stocks.add(new Stock("Fake Sirius Satellite", "SIRI", -.03, getRandomDate()));
+
+        return stocks;
+    }
+
+    public static Date getRandomDate() {
+        return getRandomDate(1970);
+    }
+
+    public static Date getRandomDate(int startYear) {
+        int year = startYear + (int) (Math.random() * 30);
+        int month = (int) (Math.random() * 12);
+        int day = (int) (Math.random() * 28);
+        GregorianCalendar calendar = new GregorianCalendar(year, month, day);
+        return calendar.getTime();
+    }
+}
+
