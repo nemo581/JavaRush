084712c790f25491aa0af47bcb89e37efbf674c6
AlRezn <alrezn@gmail.com>
2023-01-15 21:42:28 +0300

    task3608_MVC(6)

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -2,11 +2,13 @@ package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.FakeModel;
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
 
     public void onShowAllUsers() {
         model.loadUsers();
@@ -24,5 +26,9 @@ public class Controller {
 
     public void setUsersView(UsersView usersView) {
         this.usersView = usersView;
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
     }
 }
