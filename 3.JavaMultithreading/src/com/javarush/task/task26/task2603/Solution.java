b0fce6a39112f87c7a9e890d3c453c04aced6597
AlRezn <alrezn@gmail.com>
2023-01-31 20:23:23 +0300

    task2602_Убежденному убеждать других не трудно

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -7,8 +7,24 @@ import java.util.Comparator;
 */
 
 public class Solution {
+    public static class CustomizedComparator<T> implements Comparator<T> {
+        private final Comparator<T>[] comparators;
+
+        @SafeVarargs
+        public CustomizedComparator(Comparator<T>... comparators) {
+            this.comparators = comparators;
+        }
+
+        @Override
+        public int compare(T o1, T o2) {
+            for (Comparator<T> comparator : comparators) {
+                int result = comparator.compare(o1, o2);
+                if (result != 0) return result;
+            }
+            return 0;
+        }
+    }
 
     public static void main(String[] args) {
-
     }
 }
