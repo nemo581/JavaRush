a67c43bdad6afd8c5435dede632f3dcd3a26bf16
AlRezn <alrezn@gmail.com>
2023-02-06 01:13:05 +0300

    task3008_Чат (7)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,12 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     private static class Handler extends Thread {
         private Socket socket;
 
@@ -13,10 +17,21 @@ public class Server {
         }
     }
 
+    public static void sendBroadcastMessage(Message message) {
+
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
             ConsoleHelper.writeMessage("Сервер запущен.");
-            while(true) {
+            while (true) {
                 new Handler(serverSocket.accept()).start();
             }
         } catch (IOException e) {
