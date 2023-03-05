6095a82c4308829736acd26e3ddbf2de75390adc
AlRezn <alrezn@gmail.com>
2023-03-05 18:35:44 +0300

    task2710_Расставь wait-notify

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task27.task2710;
+
+public class MailServer implements Runnable {
+    private final Mail mail;
+
+    public MailServer(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        synchronized (mail) {
+            while (mail.getText() == null) {
+                try {
+                    mail.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            String name = Thread.currentThread().getName();
+            long endTime = System.currentTimeMillis();
+            System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+        }
+    }
+}
