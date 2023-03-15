0e4c9d4816d88b5c3bc14e06222ac505321560f7
AlRezn <alrezn@gmail.com>
2023-03-15 18:13:19 +0300

    task3110_Archiver (8)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,21 +1,46 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) throws Exception {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String fullArchivePath = reader.readLine();
-            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fullArchivePath));
-            String fullEndPathOfTheArchive = reader.readLine();
-            zipFileManager.createZip(Paths.get(fullEndPathOfTheArchive));
-        }
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
+        return Operation.values()[ConsoleHelper.readInt()];
+    }
+
+    public static void main(String[] args) throws Exception {
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+//            String fullArchivePath = reader.readLine();
+//            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fullArchivePath));
+//            String fullEndPathOfTheArchive = reader.readLine();
+//            zipFileManager.createZip(Paths.get(fullEndPathOfTheArchive));
+//        }
+//
+//        ExitCommand exitCommand = new ExitCommand();
+//        exitCommand.execute();
+        Operation operation = null;
+        while (operation != Operation.EXIT) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException w) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
     }
 }
