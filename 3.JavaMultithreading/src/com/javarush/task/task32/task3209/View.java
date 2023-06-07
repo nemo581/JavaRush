90f3c806102b5f9d86ce30d9f30e196a4fef82f6
AlRezn <alrezn@gmail.com>
2023-06-07 21:59:00 +0300

    task3209_HTML Editor (18)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -45,7 +45,15 @@ public class View extends JFrame implements ActionListener {
     }
 	
 	public void selectedTabChanged() {
-		
+        switch (tabbedPane.getSelectedIndex()) {
+            case (0):
+                controller.setPlainText(plainTextPane.getText());
+                break;
+            case (1):
+                plainTextPane.setText(controller.getPlainText());
+                break;
+        }
+        resetUndo();
 	}
 
     public void init() {
