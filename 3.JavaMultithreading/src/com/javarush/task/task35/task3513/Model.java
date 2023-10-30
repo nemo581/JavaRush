23eafa64eaffa80ae6997f9803dd37ac27e1499a
AlRezn <alrezn@gmail.com>
2023-10-30 15:29:12 +0300

    task3513_2048 (3)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,15 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (Tile[] gt : gameTiles) {
+            for (int k = 0; k < gameTiles.length; k++) {
+                gt[k] = new Tile();
+            }
+        }
+    }
 }
