9ed345bd34fc1ac408f513e818117782c35e2c5f
AlRezn <alrezn@gmail.com>
2023-10-31 08:33:38 +0300

    task3310_Shortener (5)

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,6 +1,14 @@
 package com.javarush.task.task33.task3310;
 
+import java.util.Set;
+
 public class Solution {
+    static Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        return null;
+    }
+    static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+        return null;
+    }
     public static void main(String[] args) {
 
     }
