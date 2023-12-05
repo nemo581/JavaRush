2f0003db85bd0a6c2f4424f8b8ffe984c74a3b80
AlRezn <alrezn@gmail.com>
2023-12-05 21:02:39 +0300

    task3310_Shortener (6)

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/HashMapStorageStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/HashMapStorageStrategy.java
@@ -1,39 +1,32 @@
 package com.javarush.task.task33.task3310.strategy;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class HashMapStorageStrategy implements StorageStrategy {
     private HashMap<Long, String> data = new HashMap<>();
 
-    @Override
     public boolean containsKey(Long key) {
         return data.containsKey(key);
     }
 
-    @Override
     public boolean containsValue(String value) {
         return data.containsValue(value);
     }
 
-    @Override
     public void put(Long key, String value) {
         data.put(key, value);
     }
 
-    @Override
-    public Long getKey(String value) {
-        Long result = null;
-        for (Map.Entry<Long, String> entry : data.entrySet()) {
-            if (entry.getValue().equals(value)) {
-                result = entry.getKey();
-            }
-        }
-        return result;
-    }
-
-    @Override
     public String getValue(Long key) {
         return data.get(key);
+    }
+
+    public Long getKey(String value) {
+        for (Long key : data.keySet()) {
+            if (data.get(key).equals(value)) {
+                return key;
+            }
+        }
+        return null;
     }
 }
